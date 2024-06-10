6dc2a15e1de8815479c812ff4eb93ce26d7302c3
ljmm2k2 <ljmm2k2@gmail.com>
2024-06-10 11:30:44 -0400

    basic set-up

---
+++ calculator/src/App.jsx
@@ -0,0 +1,10 @@
+import {useState} from 'react'
+import './App.css'
+
+function Calculator() {
+  // const [count, setCount] = useState(0)
+
+  return <>HOLA MUNDO</>
+}
+
+export default Calculator
